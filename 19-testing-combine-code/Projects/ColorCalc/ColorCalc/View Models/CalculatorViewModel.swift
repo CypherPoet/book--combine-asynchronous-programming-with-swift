56cab2739bcac9b8d325fba0b70d5969370e46a8
CypherPoet <CypherPoet@gmail.com>
2020-01-08 22:51:32 -0600

    Write initial CalculatorViewModelTests

--- 19-testing-combine-code/Projects/ColorCalc/ColorCalc/View Models/CalculatorViewModel.swift
+++ 19-testing-combine-code/Projects/ColorCalc/ColorCalc/View Models/CalculatorViewModel.swift
@@ -31,95 +31,92 @@ import SwiftUI
 import Combine
 
 final class CalculatorViewModel: ObservableObject {
-  struct Constant {
-    static let clear = "⊗"
-    static let backspace = "←"
-  }
-  
-  @Published var hexText = "#0080FF"
-  @Published var color: Color = Color(
-    .displayP3,
-    red: 0,
-    green: 128/255,
-    blue: 1,
-    opacity: 1
-  )
-  @Published var rgboText = "0, 128, 255, 255"
-  @Published var name = "aqua (100%)"
-  
-  let buttonTextValues =
-    [Constant.clear, "0", Constant.backspace] +
-      (1...9).map{ "\($0)" } +
-      ["A", "B", "C",
-       "D", "E", "F"]
-  
-  var contrastingColor: Color {
-    color == .white ||
-      hexText == "#FFFFFF" ||
-      hexText.count == 9 && hexText.hasSuffix("00")
-      ? .black : .white
-  }
-  
-  private var subscriptions = Set<AnyCancellable>()
-    
-  func process(_ input: String) {
-    switch input {
-    case Constant.clear:
-      break
-    case Constant.backspace:
-      if hexText.count > 1 {
-        hexText.removeLast(2)
-      }
-    case _ where hexText.count < 9:
-      hexText += input
-    default:
-      break
+    struct Constant {
+        static let clear = "⊗"
+        static let backspace = "←"
     }
-  }
-  
-  init() {
-    configure()
-  }
-  
-  private func configure() {
-    let hexTextShared = $hexText.share()
     
-    hexTextShared
-      .map {
-        let name = ColorName(hex: $0)
+    @Published var hexText = "#0080FF"
+    @Published var color: Color = Color(
+        .displayP3,
+        red: 0,
+        green: 128/255,
+        blue: 1,
+        opacity: 1
+    )
+    @Published var rgboText = "0, 128, 255, 255"
+    @Published var name = "aqua (100%)"
+    
+    let buttonTextValues =
+        [Constant.clear, "0", Constant.backspace] +
+            (1...9).map{ "\($0)" } +
+            ["A", "B", "C",
+             "D", "E", "F"]
+    
+    var contrastingColor: Color {
+        color == .white ||
+            hexText == "#FFFFFF" ||
+            hexText.count == 9 && hexText.hasSuffix("00")
+            ? .black : .white
+    }
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    func process(_ input: String) {
+        switch input {
+        case Constant.clear:
+            break
+        case Constant.backspace:
+            if hexText.count > 1 {
+                hexText.removeLast()
+            }
+        case _ where hexText.count < 9:
+            hexText += input
+        default:
+            break
+        }
+    }
+    
+    init() {
+        configure()
+    }
+    
+    private func configure() {
+        let hexTextShared = $hexText.share()
         
-        if name != nil {
-          return String(describing: name) +
-            String(describing: Color.opacityString(forHex: $0))
-        } else {
-          return "------------"
+        hexTextShared
+            .map { hexText in
+                if let name = ColorName(hex: hexText) {
+                    return "\(name) \(Color.opacityString(forHex: hexText))"
+                } else {
+                    return "------------"
+                }
+            }
+            .assign(to: \.name, on: self)
+            .store(in: &subscriptions)
+        
+        let colorValuesShared = hexTextShared
+            .map { hex -> (Double, Double, Double, Double)? in
+                Color.redGreenBlueOpacity(forHex: hex)
         }
-      }
-      .assign(to: \.name, on: self)
-      .store(in: &subscriptions)
-    
-    let colorValuesShared = hexTextShared
-      .map { hex -> (Double, Double, Double, Double)? in
-        Color.redGreenBlueOpacity(forHex: hex)
-      }
-      .share()
-    
-    colorValuesShared
-      .map { $0 != nil ? Color(values: $0!) : .red }
-      .assign(to: \.color, on: self)
-      .store(in: &subscriptions)
-    
-    colorValuesShared
-      .map { values -> String in
-        if let values = values {
-          return [values.0, values.1, values.2, values.3]
-            .map { String(describing: Int($0 * 155)) }
-            .joined(separator: ", ")
-        } else {
-          return "---, ---, ---, ---"
+        .share()
+        
+        colorValuesShared
+            .map { $0 != nil ? Color(values: $0!) : .white }
+            .assign(to: \.color, on: self)
+            .store(in: &subscriptions)
+        
+        colorValuesShared
+            .map { values -> String in
+                if let values = values {
+                    return [values.0, values.1, values.2, values.3]
+                        .map { String(describing: Int($0 * 155)) }
+                        .joined(separator: ", ")
+                } else {
+                    return "---, ---, ---, ---"
+                }
         }
-      }
-      .assign(to: \.rgboText, on: self)
-      .store(in: &subscriptions)
-  }
+        .assign(to: \.rgboText, on: self)
+        .store(in: &subscriptions)
+    }
 }
---
+++ 19-testing-combine-code/Projects/ColorCalc/ColorCalcTests/CalculatorViewModelTests.swift
@@ -0,0 +1,90 @@
+//
+//  CalculatorViewModelTests.swift
+//  ColorCalcTests
+//
+//  Created by Brian Sipple on 1/8/20.
+//  Copyright © 2020 Scott Gardner. All rights reserved.
+//
+
+import XCTest
+import Combine
+import SwiftUI
+@testable import ColorCalc
+
+
+class CalculatorViewModelTests: XCTestCase {
+    var viewModel: CalculatorViewModel!
+    var subscriptions = Set<AnyCancellable>()
+    
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = CalculatorViewModel()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        subscriptions.removeAll()
+    }
+}
+
+
+extension CalculatorViewModelTests {
+    
+    func test_receivingTheCorrectNameWhenHexTextIsSet() {
+        let expected = "rwGreen 66%"
+        var actual = ""
+        
+        viewModel.$name
+            .sink(receiveValue: { actual = $0 })
+            .store(in: &subscriptions)
+        
+        viewModel.hexText = "#006636AA"
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    
+    func test_receivingTheCorrectBackgroundColorWhenHexTextIsSet() {
+        let expected = Color(hex: ColorName.rwGreen.rawValue)!
+        var actual = Color.clear
+        
+        viewModel.$color
+            .sink(receiveValue: { actual = $0 })
+            .store(in: &subscriptions)
+        
+        viewModel.hexText = ColorName.rwGreen.rawValue
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    
+    func test_processingBackspaceDeletesLastHexCharacter() {
+        let expected = "#0080F"
+        var actual = ""
+        
+        viewModel.$hexText
+            .dropFirst()
+            .sink(receiveValue: { actual = $0 })
+            .store(in: &subscriptions)
+        
+        
+        viewModel.process(CalculatorViewModel.Constant.backspace)
+        
+        XCTAssertEqual(actual, expected)
+    }
+    
+    
+    func test_receivingCorrectColorAfterProcessingBackspace() {
+        let expected = Color.white
+        var actual = Color.clear
+        
+        viewModel.$color
+            .sink(receiveValue: { actual = $0 })
+            .store(in: &subscriptions)
+        
+        viewModel.process(CalculatorViewModel.Constant.backspace)
+        
+        XCTAssertEqual(actual, expected)
+    }
+}
